f621c21536e02b390a33774c5f2d42ed7e52bd0b
Liz Matee <lizziematee@gmail.com>
2018-06-05 17:02:13 +0300

    Add all changes

--- Workspace/settings.py
+++ Workspace/settings.py
@@ -87,9 +87,9 @@ WSGI_APPLICATION = 'Workspace.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'NAME': 'social',
+        'USER': 'liz',
+        'PASSWORD': 'aschenbrenner',
     }
 }
 
