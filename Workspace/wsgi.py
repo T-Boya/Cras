615b743f54e2f8534d400655f594417e93835818
T-Boya <t.osewe1@gmail.com>
2018-06-05 13:25:55 +0300

    removed invalid character in WSGI

--- Workspace/wsgi.py
+++ Workspace/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-from whitenoise.django import DjangoWhiteNoise 
+from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
@@ -17,4 +17,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Workspace.settings")
 
 application = get_wsgi_application()
 
-application = DjangoWhiteNoise(application) 
+application = DjangoWhiteNoise(application)
