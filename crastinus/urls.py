454147aca85c5bbd2f0dba4a808a19a9831a0e61
T-Boya <t.osewe1@gmail.com>
2018-06-08 12:17:26 +0300

    recreated app in heroku 2

--- crastinus/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from crastinus import views
-
-urlpatterns = [
-    url(r'^$', views.home, name='index'),
-    # url(r'^login/$', views.login, name='login') 
-]
--- crastinus/views.py
+++ crastinus/views.py
@@ -1,11 +1,3 @@
-from django.shortcuts import render, get_object_or_404,redirect
-from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse('It Works!')
-
-@login_required
-def home(request):
-    return render(request, 'home.html')
