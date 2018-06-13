e7197a23d35420cdca2ec2ce688a294a7999d6bb
T-Boya <t.osewe1@gmail.com>
2018-06-13 20:26:50 +0300

    facebook json parsing fix attempt six-ish

--- crastinus/views.py
+++ crastinus/views.py
@@ -153,7 +153,7 @@ def home(request):
         Facebook = True
         faceuid = allproviders[usedproviders.index('facebook')].uid
         faceresponse = requests.get("https://graph.facebook.com/{}/feed".format(faceuid)).json()
-        facedata = json.loads(faceresponse.content)
+        facedata = json.loads(faceresponse)
 
     else:
         print('Facebook registration failed')
